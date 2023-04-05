49add2bd6aa82ce5e4296f59aa367a1234c619c1
Aniket1523 <er.aniketkangane@gmail.com>
2023-04-05 18:55:28 +0530

    Final Birthday wish mail project

---
+++ main.py
@@ -0,0 +1,74 @@
+# # import smtplib
+# #
+# # my_email = "tpython02@gmail.com"
+# # password = "emjoacmqipomxuao"
+# #
+# # connection = smtplib.SMTP("smtp.gmail.com")
+# # connection.starttls()
+# # connection.login(user=my_email, password=password)
+# # connection.sendmail(
+# #     from_addr=my_email,
+# #     to_addrs="kanganeaniket15@gmail.com",
+# #     msg="Subject:hello\n\nThis is the body of email."
+# # )
+# # connection.close()
+#
+#
+# import datetime as dt
+# import random
+# import smtplib
+#
+#
+# days_in_week = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+# wod = dt.datetime.now().weekday()
+# with open("quotes.txt", "r") as quotes:
+#     list_quotes = quotes.readlines()
+# quote = random.choice(list_quotes)
+#
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(from_addr=my_email,
+#                     to_addrs="kanganeaniket15@gmail.com",
+#                     msg=f"Subject:Hi it's {days_in_week[wod]} \n\n {quote}"
+#                     )
+# connection.close()
+#
+#
+#
+#
+
+
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+my_email = "tpython02@gmail.com"
+password = "emjoacmqipomxuao"
+
+# # 2. Check if today matches a birthday in the birthdays.csv
+letter_templates = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+tod = dt.datetime.now()
+tod_day = tod.day
+tod_month = tod.month
+tod_year = tod.year
+
+birthday_data = pandas.read_csv("birthdays.csv")
+print(birthday_data)
+result = [row for row in
+          zip(birthday_data['name'], birthday_data['email'], birthday_data['year'], birthday_data['month'],
+              birthday_data['day']) if row[2] == tod_year and row[3] == tod_month and row[4] == tod_day]
+name = result[0][0]
+email = result[0][1]
+
+letter = random.choice(letter_templates)
+with open("./letter_templates/" + letter, "r") as letter_file:
+    letter_contents = letter_file.read()
+    new_letter = letter_contents.replace("[NAME]", name)
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs=email, msg=f"Subject: Birthday Wish \n\n {new_letter}")
+connection.close()
